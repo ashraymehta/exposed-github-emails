924d84bd0743959e47b9d5a512b4f33e32882196
ashray <ashraymehta@users.noreply.github.com>
2020-08-15 18:36:16 +0530

    Added bootstrap progress bar for rate-limit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: `<router-outlet></router-outlet>`,
-    styles: []
+    template: `
+      <div class="container h-100">
+        <router-outlet></router-outlet>
+      </div>`
 })
 export class AppComponent {
-    title = 'exposed-github-emails';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AppRoutingModule } from './app-routing.module';
-import { RateLimitComponent } from './rate-limit/rate-limit.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import {NgbProgressbarModule} from '@ng-bootstrap/ng-bootstrap';
+import {RateLimitComponent} from './rate-limit/rate-limit.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    RateLimitComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        RateLimitComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgbProgressbarModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
